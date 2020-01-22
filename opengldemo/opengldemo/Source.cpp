d2ac96980632087768c3869fb1ed97a3fc6e4f33
liuwei <wei.liu@showself.com>
2020-01-22 19:01:32 +0800

    first

---
+++ opengldemo/opengldemo/Source.cpp
@@ -0,0 +1,64 @@
+//#include "GLFW/"
+//typedef void(*GL_GENBUFFERS)(GLsizei, GLuint*);
+//GL_GENBUFFERS glGenbuffers = (GL_GENBUFFERS)wglGetProcAddress("glGenBuffers");
+//GLuint buffer;
+//glGenbuffers(1, &buffer);
+//#define GLEW_STATIC
+//#define GLEW_STATIC
+#pragma comment( linker, "/subsystem:windows /entry:mainCRTStartup")
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f
+};
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpengl", NULL, NULL);
+	if (window == NULL){
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		std::cout << "failed initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)){
+		processInput(window);
+		//----------------------
+		glClearColor(0.2f, 0.3f, 0.3f, 0.5f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//----------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
